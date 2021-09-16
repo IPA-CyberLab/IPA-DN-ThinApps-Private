57a7c4f76ab23504b5f550f3f7d526ef7173b158
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-09-16 22:47:25 +0900

    説明の更新

--- src/Vars/Vars.cs
+++ src/Vars/Vars.cs
@@ -14,13 +14,19 @@
 
 public static class Vars
 {
-    // ココを書換えます
+    // シン・テレワークシステム プライベート版構築者向け TODO:
+    // 以下は、ソフトウェア中の表示文字列である。適宜書き換えること。
     public static readonly string ProductName = "Thin Telework System Private Version by NTT-EAST and IPA";
     public static readonly string CompanyName = "NTT-East and IPA Anti-Corona Virus Telework Special Project";
     public static readonly string Copyright = "Information-technology Promotion Agency, Nippon Telegraph and Telephone East Corporation, and all contributors. All rights reserved.";
+
+    // シン・テレワークシステム プライベート版構築者向け TODO:
+    // 以下の項目の "Private" の文字列は、必ず変更し、ユニーク化すること。
+    // 詳しくは、構築マニュアル「8-2. アプリケーションの ID (AppId) の変更によるユニーク化」を参照すること。
+    // この値は、Vars.h の APP_ID_PREFIX の文字列と一致させる必要がある。
     public static readonly string APP_ID_PREFIX = "Private";
 
-    // おまけ
+    // 以下は、このままでよい。変更する必要はない。
     public static readonly string DistibutionPackagePrefix = APP_ID_PREFIX + "_";
 }
 
--- src/Vars/Vars.h
+++ src/Vars/Vars.h
@@ -14,24 +14,51 @@
 
 #include "VarsCurrentBuildInfo.h"
 
+// シン・テレワークシステム プライベート版構築者向け TODO:
+// 以下の 2 つの項目の "Private" の文字列は、必ず変更し、ユニーク化すること。
+// 詳しくは、構築マニュアル「8-2. アプリケーションの ID (AppId) の変更によるユニーク化」を参照すること。
 #define APP_ID_PREFIX					"Private"
 #define APP_ID_PREFIX_UNICODE			L"Private"
 
+// シン・テレワークシステム プライベート版構築者向け TODO:
+// 以下の "9825" (初期状態) の数字は、必ず変更し、ユニーク化すること。
+// 詳しくは、構築マニュアル「8-2. アプリケーションの ID (AppId) の変更によるユニーク化」を参照すること。
 #define DS_RPC_PORT						9825
 
+// シン・テレワークシステム プライベート版構築者向け TODO:
+// 以下の 4 つの各文字列内の "Private" という文字列の部分は、必ず変更し、ユニーク化すること。
+// 詳しくは、構築マニュアル「8-2. アプリケーションの ID (AppId) の変更によるユニーク化」を参照すること。
 #define DESK_PUBLISHER_NAME_ANSI		"Thin Telework System Private Version"
-
 #define	DESK_PRODUCT_NAME_SUITE			"Thin Telework System Private Version"
 #define	DESK_PRODUCT_NAME_SUITE_UNICODE		L"Thin Telework System Private Version"
 #define DESK_PUBLISHER_NAME_UNICODE		L"Thin Telework System Private Version"
 
 
-// 以下は必要に応じていじること
-#define DESK_LOCALHOST_DUMMY_FQDN		"%s.secure.cyber.ipa.go.jp"
-#define DESK_LOCALHOST_DUMMY_FQDN_V6	"%s.secure6.cyber.ipa.go.jp"
+// シン・テレワークシステム プライベート版構築者向け TODO:
+// 「DESK_LOCALHOST_DUMMY_FQDN」と「DESK_LOCALHOST_DUMMY_FQDN_V6」
+// は、以下のサンプルのドメイン名を設定したままでも良いが、
+// できるだけ自前ドメインに変更すること。
+// この場合、"%s" の部分に任意の文字列が入った場合に
+// 「DESK_LOCALHOST_DUMMY_FQDN」については 127.0.0.1 (IPv4 アドレス) が、
+// 「DESK_LOCALHOST_DUMMY_FQDN_V6」については ::1 (IPv6 アドレス) が回答されるように
+// DNS サーバーのゾーンファイルを設定すること。
+#define DESK_LOCALHOST_DUMMY_FQDN		"%s.secure.ipantt.net"
+#define DESK_LOCALHOST_DUMMY_FQDN_V6	"%s.secure6.ipantt.net"
+
+// シン・テレワークシステム プライベート版構築者向け TODO:
+// これは、シン・テレワークシステム クライアントの起動時にアップデートチェックを行なうための
+// 最新ビルド情報の提供ファイル (HTTP で text/plain 応答がある) の URL である。
+// 表記方法は、以下の URL のサンプルファイルを参照すること。
+// 以下の URL は、シン・テレワークシステム パブリック版 https://telework.cyber.ipa.go.jp/ の
+// 公式クライアントが参照するものである。
+// ここで、テキストファイルの内容は 1 行目のコメントのとおり容易に理解することが可能と思われるが、
+// "family" 列には上記の APP_ID_PREFIX の文字列に続けて "ThinClient" という文字列が付加された
+// ものが指定される必要がある。
+// すなわち、APP_ID_PREFIX 文字列が "Abc" の場合、応答テキストの "family" 列が
+// "AbcThinClient" となっている行が応答される必要がある。
 #define	UPDATE_SERVER_URL_GLOBAL		"https://update-check.dynamic-ip.thin.cyber.ipa.go.jp/update/?family=%s&software=%s&mybuild=%u&lang=%s"
 
-// URDP ポート番号変更
+// URDP ポート番号変更 (変更しないこと)
 #undef	DS_URDP_PORT
 #define DS_URDP_PORT					3459
 
