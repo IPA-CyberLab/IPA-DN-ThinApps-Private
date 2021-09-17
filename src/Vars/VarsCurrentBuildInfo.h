1bca19a5df0c17cc2c4e54670970411af23b3091
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-09-18 00:29:21 +0900

    BUILD_NUMBER 9910 VERSION 19 BUILD_NAME private.beta8preview2 BUILD_DATE 20210917_234934

--- src/Vars/VarsCurrentBuildInfo.h
+++ src/Vars/VarsCurrentBuildInfo.h
@@ -16,7 +16,7 @@
 #define	CEDAR_VER					19
 
 // Build Number
-#define	CEDAR_BUILD					9909
+#define	CEDAR_BUILD					9910
 
 // Specify the name of the person in charge building
 #ifndef	BUILDER_NAME
@@ -31,12 +31,12 @@
 // Specifies the build date
 #define	BUILD_DATE_Y		2021
 #define	BUILD_DATE_M		9
-#define	BUILD_DATE_D		16
-#define	BUILD_DATE_HO		22
-#define	BUILD_DATE_MI		13
-#define	BUILD_DATE_SE		49
+#define	BUILD_DATE_D		17
+#define	BUILD_DATE_HO		23
+#define	BUILD_DATE_MI		49
+#define	BUILD_DATE_SE		34
 
 // IPA-DN-Ultra library git commit id
-#define ULTRA_COMMIT_ID		"22dd63f34f284111dbc4d10dc9e9b493b5332839"
+#define ULTRA_COMMIT_ID		"2d610ba1dedec2a57a67cadee924519048a19387"
 #define ULTRA_VER_LABEL		"beta8preview2"
 
