78b363132d37583a603bbce6449b0798b7c614a8
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-10-14 10:32:54 +0900

    test

--- src/Vars/VarsCurrentBuildInfo.h
+++ src/Vars/VarsCurrentBuildInfo.h
@@ -17,7 +17,7 @@
 #define	CEDAR_VER					21
 
 // Build Number
-#define	CEDAR_BUILD					9920
+#define	CEDAR_BUILD					9921
 
 // Specify the name of the person in charge building
 #ifndef	BUILDER_NAME
@@ -26,18 +26,18 @@
 
 // Specify the location to build
 #ifndef	BUILD_PLACE
-#define	BUILD_PLACE			"pc38"
+#define	BUILD_PLACE			"pc34"
 #endif	// BUILD_PLACE
 
 // Specifies the build date
 #define	BUILD_DATE_Y		2022
-#define	BUILD_DATE_M		2
-#define	BUILD_DATE_D		5
-#define	BUILD_DATE_HO		1
-#define	BUILD_DATE_MI		15
-#define	BUILD_DATE_SE		28
+#define	BUILD_DATE_M		10
+#define	BUILD_DATE_D		14
+#define	BUILD_DATE_HO		10
+#define	BUILD_DATE_MI		30
+#define	BUILD_DATE_SE		55
 
 // IPA-DN-ThinLib library git commit id
-#define THINLIB_COMMIT_ID		"201ae56fdad3ca57484810e92ae9abec649f161b"
-#define THINLIB_VER_LABEL		"beta8preview7"
+#define THINLIB_COMMIT_ID		"44f1ed1b0d3bd0194229d7e79a7da1ad6422c53e"
+#define THINLIB_VER_LABEL		"beta8preview9"
 
