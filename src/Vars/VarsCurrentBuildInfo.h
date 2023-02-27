1339cf30b6cd48b66b3c3f2e15fe5d082dcba4f1
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2023-02-27 22:18:19 +0900

    BUILD_NUMBER 9923 VERSION 22 BUILD_NAME private.rc1 BUILD_DATE 20230227_221801

--- src/Vars/VarsCurrentBuildInfo.h
+++ src/Vars/VarsCurrentBuildInfo.h
@@ -17,7 +17,7 @@
 #define	CEDAR_VER					22
 
 // Build Number
-#define	CEDAR_BUILD					9922
+#define	CEDAR_BUILD					9923
 
 // Specify the name of the person in charge building
 #ifndef	BUILDER_NAME
@@ -26,18 +26,18 @@
 
 // Specify the location to build
 #ifndef	BUILD_PLACE
-#define	BUILD_PLACE			"pc34"
+#define	BUILD_PLACE			"pc37"
 #endif	// BUILD_PLACE
 
 // Specifies the build date
 #define	BUILD_DATE_Y		2023
-#define	BUILD_DATE_M		1
-#define	BUILD_DATE_D		19
-#define	BUILD_DATE_HO		23
-#define	BUILD_DATE_MI		33
-#define	BUILD_DATE_SE		52
+#define	BUILD_DATE_M		2
+#define	BUILD_DATE_D		27
+#define	BUILD_DATE_HO		22
+#define	BUILD_DATE_MI		18
+#define	BUILD_DATE_SE		1
 
 // IPA-DN-ThinLib library git commit id
-#define THINLIB_COMMIT_ID		"9d80e005808164f63d07b2d0642d13546ac7a3eb"
+#define THINLIB_COMMIT_ID		"2d3c4524f2cb4d4aacdd03e21c17ed1b6a017fc3"
 #define THINLIB_VER_LABEL		"rc1"
 
